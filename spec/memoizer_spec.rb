15d1e94ebcf11f0bb9d1086ddc9145e5feda592b
Jacek Szarski <jacek.szarski@gmail.com>
2017-05-17 11:08:45 +0200

    Test the Interactors::ImportantThing as a starting point

---
+++ spec/memoizer_spec.rb
@@ -0,0 +1,46 @@
+require './spec/spec_helper'
+
+context "When Interactors::ImportantThing#call sums arguments and has a side effect" do
+  before(:all) do
+    module Interactors
+      class Base
+      end
+
+      class ImportantThing < Base
+        def initialize(*args)
+          # does whatever
+        end
+
+        def side_effect
+          # some undesired computation-heavy side effect
+        end
+
+        def call(*args)
+          side_effect
+          # Let's add the arguments up,
+          # Array#sum is not implemented in pure ruby
+          args.inject(0) { |result, argument| result + argument }
+        end
+      end
+    end
+  end
+
+  subject { Interactors::ImportantThing.new }
+
+  context "When there is no memoization" do
+    context "When different arguments are passed" do
+      it "returns different values" do
+        expect(subject.call(1,2,3)).to eq(6)
+        expect(subject.call(4,5,6)).to eq(15)
+      end
+    end
+
+    context "when the same arguments are passed" do
+      it "runs the side effect every time" do
+        expect(subject).to receive(:side_effect).twice
+        subject.call(1,2,3)
+        subject.call(1,2,3)
+      end
+    end
+  end
+end
